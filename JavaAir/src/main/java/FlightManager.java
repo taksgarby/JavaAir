e984141a4c5c92e0bd22c188ea3e091735efab2a
Takako Onozuka <onozukatakako@gmail.com>
2022-12-04 17:20:58 +0000

    extension

--- JavaAir/src/main/java/FlightManager.java
+++ JavaAir/src/main/java/FlightManager.java
@@ -3,29 +3,19 @@ import Plane.PlaneType;
 
 public class FlightManager {
 
-    private Plane planeInstance;
+    private Flight myFlight;
 
-    public FlightManager(Plane planeInstance) {
-        this.planeInstance = planeInstance;
+    public FlightManager(Flight myFlight) {
+        this.myFlight = myFlight;
     }
+
 
 
     public int getBaggageWeightAllowance() {
 
-        int planeCapacity = planeInstance.getType().getCAPACITY();
-        int planeTotalWeight = planeInstance.getType().getTOTAL_WEIGHT();
-        int baggageWeightAllowance = Math.round(planeTotalWeight / planeCapacity);
+        return myFlight.getPlane().getTotalWeight() / myFlight.getPlane().getCapacity();
 
-        return baggageWeightAllowance;
 
-        // PlaneType is a class, not an object. So PlaneType.getCAPACITY would be a class level
-        // method that returns the same thing every time and could not refer to a particular
-        // instasnce of the class. If you had defined
-        // PlaneType myPlane
-        // and then ran myPlane.getCAPACITY()
-        // that would have returned the capacity of a particular instance of a plane (an object)
-        // private Pilot pilot; is defining an instance of a pilot
-        // return pilot.getName(); is returning the name of that instance
 
     }
 
--- JavaAir/src/test/java/FlightManagerTest.java
+++ JavaAir/src/test/java/FlightManagerTest.java
@@ -1,21 +1,38 @@
-//import Plane.Plane;
-//import Plane.PlaneType;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class FlightManagerTest {
-//
-//    FlightManager flightManager;
-//
-//    @Before
-//    public void before() {
-//        Plane plane1 = new Plane(PlaneType.AIRBUS_220);
-//    }
-//
-//    @Test
-//    public void calculateBaggageWeightAllowance() {
-//        assertEquals(2, flightManager.getBaggageWeightAllowance());
-//    }
-//}
+
+import Crew.CabinCrewMember;
+import Crew.Pilot;
+import Crew.Rank;
+import Plane.Plane;
+import Plane.PlaneType;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlightManagerTest {
+
+    FlightManager flightManager;
+
+    @Before
+    public void before() {
+        Pilot pilot1 = new Pilot("Grinch", Rank.PILOT, "1234");
+        ArrayList<CabinCrewMember> cabinCrewMembers1 = new ArrayList<>();
+        CabinCrewMember flightAttendant1 = new CabinCrewMember("Rudolf Rednose", Rank.FLIGHT_ATTENDANT);
+        cabinCrewMembers1.add(flightAttendant1);
+        ArrayList<Passenger> bookedPassengers1 = new ArrayList<>();
+        Passenger passenger1 = new Passenger("Santa Clause", 3);
+        bookedPassengers1.add(passenger1);
+        Plane plane1 = new Plane(PlaneType.AIRBUS_220);
+        Flight flight1 = new Flight
+                (pilot1, cabinCrewMembers1, bookedPassengers1, plane1, "AB123", "Tokyo", "GLA", "12:00");
+        flightManager = new FlightManager(flight1);
+
+    }
+
+    @Test
+    public void calculateBaggageWeightAllowance() {
+        assertEquals(2, flightManager.getBaggageWeightAllowance());
+    }
+}
